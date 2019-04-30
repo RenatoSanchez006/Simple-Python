5ce8b5da1959760b2262b76d4c5eaa6d0077e5cd
marcegarzab <marcegarzab@gmail.com>
2019-04-30 15:01:49 -0500

    Make loginSignup page

--- js/simplePython.js
+++ js/simplePython.js
@@ -29,11 +29,48 @@ $('#advancement')
     }
   })
 ;
-
 /*
 {percent}	Current percentage
 {value}	Current value
 {total}	Total value
 {left}	Distance to total, or % progress left
 */
+
+//Log in / Sign up button redirect
+$("#login").click(function(){
+  window.location.href = "./";
+});
+
+$("#signup").click(function(){
+  window.location.href = "./";
+});
+
+$(document)
+  .ready(function() {
+    $('#login-form"')
+      .form({
+        fields: {
+          email: {
+            identifier  : 'email', //check
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please enter your e-mail'
+              }
+            ]
+          },
+          password: {
+            identifier  : 'password',
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please enter your password'
+              }
+            ]
+          }
+        }
+      })
+    ;
+  })
+;
 
