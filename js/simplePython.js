591f777d4791ed4f0812527ffa52c5063a4f31bd
marcegarzab <marcegarzab@gmail.com>
2019-04-30 10:53:10 -0500

    Finish aboutUs page and dashboard page, Add to index page

--- js/simplePython.js
+++ js/simplePython.js
@@ -6,3 +6,34 @@ $('.item').click(function () {
 	$('.active').removeClass('active');
 	$(this).addClass('active');
 });
+
+//Progress bars:
+$('.progress-bar')
+  .progress('increment')
+;
+
+$('#score')
+  .progress({
+    text: {
+      active  : 'Done {value} of {total} problems correctly',
+      success : '{total} Problems Done Correctly! '
+    }
+  })
+;
+
+$('#advancement')
+  .progress({
+    text: {
+      active  : 'Done {value} of {total} total problems',
+      success : '{total} Problems Finished!'
+    }
+  })
+;
+
+/*
+{percent}	Current percentage
+{value}	Current value
+{total}	Total value
+{left}	Distance to total, or % progress left
+*/
+
